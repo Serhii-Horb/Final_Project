1db4d38dc51f973e656902cd68926e3bbb43c982
Serhii Horb <serhii.horb.ua@gmail.com>
2024-06-19 11:02:10 +0200

    First sprint

---
+++ src/main/java/com/example/final_project/mapper/Mappers.java
@@ -0,0 +1,9 @@
+package com.example.final_project.mapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mappers {
+
+}
---
+++ src/main/resources/db/changelog/changeSets/v0.1/insertProducts.sql
@@ -0,0 +1,11 @@
+-- liquibase formatted sql
+
+-- changeset serhii:insert_products
+
+INSERT INTO products (Name, Description, Price, CategoryId, ImageURL, DiscountPrice,  CreatedAt, UpdatedAt, Quantity)
+VALUES
+    ('IPA Beer', 'A hoppy IPA beer with citrus flavors.', '6.99', 1, 'https://example.com/images/ipa_beer.jpg', '5.99', NOW(), NOW(), 1),
+    ('Red Wine', 'A full-bodied red wine with notes of blackberry and oak.', '19.99', 2, 'https://example.com/images/red_wine.jpg', '17.99', NOW(), NOW(), 2),
+    ('Whiskey', 'A smooth whiskey aged 12 years in oak barrels.', '39.99', 3, 'https://example.com/images/whiskey.jpg', '35.99', NOW(), NOW(), 3),
+    ('Coffee Liqueur', 'A rich coffee liqueur perfect for cocktails.', '24.99', 4, 'https://example.com/images/coffee_liqueur.jpg', '22.99', NOW(), NOW(), 4),
+    ('Apple Cider', 'A crisp and refreshing apple cider.', '9.99', 5, 'https://example.com/images/apple_cider.jpg', '8.99', NOW(), NOW(), 5);
