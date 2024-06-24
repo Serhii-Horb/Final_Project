a0a87e6f40397cbefe71da74ef8ebea3e0359af3
daniil <daniilkienko@mail.ru>
2024-06-24 14:23:08 +0500

    Products and Categories 1st sprint Daniil

---
+++ src/main/java/com/example/final_project/dto/CategoryDto.java
@@ -0,0 +1,16 @@
+package com.example.final_project.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryDto {
+    private long categoryId;
+    private String name;
+}
---
+++ src/main/java/com/example/final_project/entity/Product.java
@@ -0,0 +1,59 @@
+package com.example.final_project.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ProductId")
+    private long productId;
+
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "Description")
+    private String description;
+
+    @Column(name = "Price")
+    private BigDecimal price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CategoryId", nullable = false)
+    private Category category;
+
+    @Column(name = "ImageURL")
+    private String imageURL;
+
+    @Column(name = "DiscountPrice")
+    private BigDecimal discountPrice;
+
+    @Column(name = "CreatedAt")
+    private Timestamp createdAt;
+
+    @Column (name = "UpdatedAt")
+    private Timestamp updatedAt;
+
+//    @OneToMany (mappedBy = "product", cascade = CascadeType.ALL)
+//    private Set<CartItem> cartItem = new HashSet<>();
+
+//    @OneToMany (mappedBy =  "product", cascade = CascadeType.ALL)
+//    private Set<OrderItems> orderItems = new HashSet<>();
+
+//    @OneToMany (mappedBy = "product", cascade = CascadeType.ALL)
+//    private Set<Favorites> favorites = new HashSet<>();
+
+}
